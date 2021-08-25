f898a9403c7f191626f5833d14dc4605a48fd7d9
Dani <chisteleco@gmail.com>
2021-08-25 17:17:42 +0200

    added comments for clarification

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
 let express = require('express');
 let app = express();
 
+
+//API REST METHODS
 app.get('/', function(req, res) {
   res.send('Hola Mundo!');
 });
 
+
+
+//LAUNCH LISTENING
 app.listen(3000, function() {
   console.log('Aplicación ejemplo, escuchando el puerto 3000!');
 });
