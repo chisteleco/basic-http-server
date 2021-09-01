11ca2960f3bdc2c615319c73caf5111d35302aa0
Dani <chisteleco@gmail.com>
2021-09-01 17:52:59 +0200

    deleted dummy tests

--- test/validator.js
+++ test/validator.js
@@ -7,30 +7,6 @@ let expect = chai.expect();
 let request = chai.request();
 
 
-
-
-
-
-//const validator = require('../src/initComms')
-describe("initComms isInit()", () => {
-	it("should return true", ()=> {
-		chai.expect(true).to.be.true
-	})
-	it("should return false when the number is less 10", () => {
-		chai.expect(false).to.be.false
-	})
-	it("should return false when the number is equal to 10", () => {
-		chai.expect(false).to.be.false
-	})
-	it("should return false when the number is equal to 70", () => {
-		chai.expect(false).to.be.false
-	})
-	it("should return false when the number is greater than 70", () => {
-		chai.expect(false).to.be.false
-	})
-})
-
-
 describe('HTTP Comms: ',()=>{
  it('should initialize web page', (done) => {
  chai.request("http://localhost:3000")
