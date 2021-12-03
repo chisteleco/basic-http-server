624ad6015e171133fc9ba1fcad200f2e758a85f9
Dani <chisteleco@gmail.com>
2021-12-03 08:31:25 +0100

    added pictures

--- app/routes/decRoutes.js
+++ app/routes/decRoutes.js
@@ -9,56 +9,64 @@ router.get('/', function (req, res) {
     let date = _date.toDateString().slice(4);
     console.log(date);
 
+
     switch (date) {
 
-        case 'Dec 3 2021':
-            res.json({ 'msg': 'Cumplea\361os feliz!!!Cumplea\361os feliz!!!', 'img': '/Images/30.jpg', 'color': 'bisque', 'secret': 'Busca debajo de la cama', 'otherFile': '/OtherFiles/grabacion.aac' });
+        case 'Dec 3 2021'://pista sakale debajo de la cama
+            res.json({ 'msg': 'Cumplea\361os feliz!!!Cumplea\361os feliz!!!', 'img': '/Images/30.jpg', 'color': 'lightgreen', 'secret': 'Busca debajo de la cama', 'otherFile': '/OtherFiles/grabacion.aac' });
             break;
-        case 'Dec 4 2021':
-            res.json({ 'msg': 'Lo importante en la vida es aprender a tomar las riendas , elegir el camino, y disfrutar el paisaje', 'img': '/Images/caballos.jpg', 'color': 'bisque', 'secret': 'ponte ropa cómoda!', 'otherFile': '/OtherFiles/poni.mp3' });
+        case 'Dec 4 2021'://caballitos
+            res.json({ 'msg': 'Lo importante en la vida es aprender a tomar las riendas , elegir el camino, y disfrutar el paisaje', 'img': '/Images/caballos.jpg', 'color': 'bisque', 'secret': 'ponte ropa c\u00F3moda!', 'otherFile': '/OtherFiles/poni.mp3' });
             break;
-        case 'Dec 5 2021':
-            res.json({ 'msg': 'Hoy toca..... churros!!', 'img': '/Images/churros.jpg', 'color': 'DarkGoldenRod'});
+        case 'Dec 5 2021'://churros
+            res.json({ 'msg': 'Hoy toca..... churros!!', 'img': '/Images/churros.jpg', 'color': 'DarkGoldenRod', 'secret': 'churros para mi churri ^^'});
             break;
-        case 'Dec 6 2021':
+        case 'Dec 6 2021': //masaje
             res.json({ 'msg': 'Hoy has ganado..... un masaje!!', 'img': '/Images/10.gif', 'color': 'DarkGoldenRod' });
             break;
-        case 'Dec 7 2021':
+        case 'Dec 7 2021': //pista detrás de halcón
             res.json({ 'img': '/Images/gelieg.png', 'color': 'black' });
             break;
-        case 'Dec 8 2021':
+        case 'Dec 8 2021'://TODO
+            res.json({ 'msg':'Hoy es 8!','color':'blue'});
             break;
-        case 'Dec 9 2021':
+        case 'Dec 9 2021': //piruletas!
+            res.json({ 'msg':'Te acuerdas? :)','img':'/Images/piruletas.jfif','color': 'lightblue', 'secret':'Hay 5 escondidas por la casa. Dulce b\u00F9squeda!!!'});
             break;
-        case 'Dec 10 2021':
-            res.json({ 'msg': 'Hoy me toca a mi!! Pero qué feliz soy de poder celebrarlo a tu lado :) ', 'img': '/Images/9.jpg', 'color': 'DarkGoldenRod' });
+        case 'Dec 10 2021'://mi cumple
+            res.json({ 'msg': 'Hoy me toca a mi!! Pero qu\u00E9 feliz soy de poder celebrarlo a tu lado :) ', 'img': '/Images/9.jpg', 'color': 'DarkGoldenRod' });
             break;
-        case 'Dec 11 2021':
+        case 'Dec 11 2021': //debajo del gato  hay algo
+            res.json({ 'msg': 'Y ooootra vez, no aprendo!!! NO DOY UNA!!!Siempre lo coloco mal', 'img':'/Images/Ignatius.png','color': 'DarkGoldenRod' });
             break;
-        case 'Dec 12 2021':
+        case 'Dec 12 2021'://Peli
+            res.json({ 'msg': 'Tras una buena comilona, nada mejor que sof\u00E1 y peli. Tu eliges :)', 'img': '/Images/movies.jpg', 'color': 'lightblue' });
             break;
-        case 'Dec 13 2021':
+        case 'Dec 13 2021'://TODO
             break;
-        case 'Dec 14 2021':
+        case 'Dec 14 2021'://TODO
+            res.json({ 'msg': 'El secreto de la juventud?', 'img': '/Images/5.jpg', 'color': 'lightgreen'});
             break;
-        case 'Dec 15 2021':
+        case 'Dec 15 2021'://TODO
             break;
-        case 'Dec 16 2021':
-            res.json({ 'msg': 'Hoy toca, vacaciones navideñas! viva!!', 'img': '/Images/alsacia.jpg', 'color': 'DarkGoldenRod' });
+        case 'Dec 16 2021': //Viaje!!!
+            res.json({ 'msg': 'Hoy toca, vacaciones navide\361as! viva!!', 'img': '/Images/alsacia.jpg', 'color': 'DarkGoldenRod' });
             break;
-        case 'Dec 17 2021':
+        case 'Dec 17 2021'://TODO
             break;
-        case 'Dec 18 2021':
+        case 'Dec 18 2021'://TODO
             break;
-        case 'Dec 19 2021':
+        case 'Dec 19 2021'://TODO
             break;
-        case 'Dec 20 2021':
+        case 'Dec 20 2021'://TODO
             break;
-        case 'Dec 21 2021':
+        case 'Dec 21 2021'://TODO
             break;
-        case 'Dec 22 2021':
+        case 'Dec 22 2021'://TODO
+            res.json({ 'msg': 'El premio es compartir la vida contigo :)', 'img': '/Images/calvo.jpg', 'color': 'DarkGoldenRod' });
             break;
         case 'Dec 23 2021':
+            res.json({ 'msg': '\u00BFRecuerdas el c\u00F3digo oculto de ayer?', 'color': 'DarkGoldenRod' });
             break;
         case 'Dec 24 2021':
             break;
